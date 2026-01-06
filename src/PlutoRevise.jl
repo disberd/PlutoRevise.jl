84c86eace013974fcbf9a5b89103fcf35f6c1027
Alberto Mengali <disberd@gmail.com>
2026-01-06 15:49:24 +0100

    initial commit

---
+++ src/PlutoRevise.jl
@@ -0,0 +1,8 @@
+module PlutoRevise
+    using Revise
+    using HypertextLiteral
+    using AbstractPlutoDingetjes
+    using AbstractPlutoDingetjes.Display: Display, with_js_link, published_to_js
+
+
+end # module PlutoRevise
